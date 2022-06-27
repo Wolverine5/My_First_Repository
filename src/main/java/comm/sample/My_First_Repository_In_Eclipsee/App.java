9ed363af092c5e9b5821a0e0a629990ecb25cfa6
Aditya <adiajadhav@gmail.com>
2022-06-27 09:48:30 +0530

    Initial Commit

---
+++ src/main/java/comm/sample/My_First_Repository_In_Eclipsee/App.java
@@ -0,0 +1,13 @@
+package comm.sample.My_First_Repository_In_Eclipsee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/comm/sample/My_First_Repository_In_Eclipsee/Login_Test.java
@@ -0,0 +1,49 @@
+package comm.sample.My_First_Repository_In_Eclipsee;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Login_Test {
+
+	public static WebDriver driver;
+
+	@BeforeTest
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver","C:\\Selenium_Drivers\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		//System.out.println("Launching Browser");
+	}
+	@Test
+	public void doLogin() {
+		
+		System.out.println("Executing Login Test");
+		driver.get("https://www.facebook.com/login/");
+		
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.findElement(By.cssSelector("#email")).sendKeys("adiajadhav@gmail.com");
+
+		driver.findElement(By.cssSelector("#pass")).sendKeys("abcd");
+
+		driver.findElement(By.cssSelector("#loginbutton")).click();
+
+	}
+	
+	@AfterTest
+	public void QuitDriver(){
+		if(driver!=null)
+			driver.close();
+	}
+}
+
+	
+
+
+
+
